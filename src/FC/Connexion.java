19c5e71711791b518e96135e0c169de6fdb17b9d
neilerua72 <neilerua72@gmail.com>
2019-02-01 16:54:08 +0100

    Implémentation de la classe Connexion
    Début de l'implémentation de la classe connexion. Ajout du constructeur

--- src/FC/Connexion.java
+++ src/FC/Connexion.java
@@ -1,5 +1,11 @@
 package FC;
 
 public class Connexion {
+    private boolean verif;
+    private TypeConnexion type;
 
+    public Connexion(boolean verif, TypeConnexion type){
+        this.verif=verif;
+        this.type=type;
+    }
 }
