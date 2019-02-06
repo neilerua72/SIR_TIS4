7f770f43aa1bfb8d6202245a52d1708d152622e9
glebby1 <glebby3@gmail.com>
2019-02-06 13:39:17 +0100

    interface connexion

--- src/IU/acceuil_medecin_main.java
+++ src/IU/acceuil_medecin_main.java
@@ -1,12 +1,10 @@
-package IU.acceuil_medecin;
+package IU;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class acceuil_medecin_main extends Application {
 
