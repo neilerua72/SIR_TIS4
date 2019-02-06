bd5341f157a1087b662d50fcbf6b188cf41f3311
hugdude <lfdh69@yahoo.fr>
2019-02-06 15:18:54 +0100

    ajout bd hugo

--- src/BD/Connect.java
+++ src/BD/Connect.java
@@ -13,17 +13,29 @@ public class Connect {
         }
 
         /* Connexion à la base de données */
-        String url = "jdbc:mysql://localhost:3306/bdsir";
-        String utilisateur = "root";
+        String url = "jdbc:mysql://localhost:8080/bdsir";
+        String utilisateur = "root@localhost ";
         String motDePasse = "";
         Connection connexion = null;
+
         try {
             connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
 
             /* Ici, nous placerons nos requêtes vers la BDD */
+
+            /* Création de l'objet gérant les requêtes */
+            Statement statement = connexion.createStatement();
+            /* Exécution d'une requête de lecture */
+            ResultSet resultat = statement.executeQuery( "SELECT id FROM Connexion;" );
+            /* Récupération des données du résultat de la requête de lecture */
+            while ( resultat.next() ) {
+                System.out.println("test");
+                System.out.println(resultat.getInt( "id" ));
+            }
             /* ... */
 
         } catch ( SQLException e ) {
+            System.out.println("erreur 2");
             /* Gérer les éventuelles erreurs ici */
         } finally {
             if ( connexion != null )
