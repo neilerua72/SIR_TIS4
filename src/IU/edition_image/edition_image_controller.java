acf3109221dd2ea7782b305a57f6bbdcb5bd951d
vaeat <vaea.tesan@yahoo.com>
2019-02-14 16:17:09 +0100

    Mise en groupe des toggle buttons  page acceuil

--- src/IU/edition_image/edition_image_controller.java
+++ src/IU/edition_image/edition_image_controller.java
@@ -7,68 +7,83 @@ package IU.edition_image;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.effect.Lighting;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class edition_image_controller {
+import javax.swing.*;
 
-        @FXML // ResourceBundle that was given to the FXMLLoader
-        private ResourceBundle resources;
-
-        @FXML // URL location of the FXML file that was given to the FXMLLoader
-        private URL location;
-
-        @FXML // fx:id="button_inversionGris"
-        private Button button_inversionGris; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_symetrieHorizontale"
-        private Button button_symetrieHorizontale; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_refaire"
-        private Button button_refaire; // Value injected by FXMLLoader
-
-        @FXML // fx:id="imageView_editionImage"
-        private ImageView imageView_editionImage; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_symetrieVerticale"
-        private Button button_symetrieVerticale; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_augmenterContraste"
-        private Button button_augmenterContraste; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_diminuerContraste"
-        private Button button_diminuerContraste; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_ajoutCommentaire"
-        private Button button_ajoutCommentaire; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_supprimer"
-        private Button button_supprimer; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_annuler"
-        private Button button_annuler; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_rotationGauche"
-        private Button button_rotationGauche; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_valider"
-        private Button button_valider; // Value injected by FXMLLoader
-
-        @FXML // fx:id="button_rotationDroite"
-        private Button button_rotationDroite; // Value injected by FXMLLoader
-
-        // @FXML
-        //  void 97979700(ActionEvent event) {
-        //
-        // }
+public class edition_image_controller implements Initializable {
 
 
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
 
-        @FXML // This method is called by the FXMLLoader when initialization is complete
-        void initialize() {
-            assert button_inversionGris != null : "fx:id=\"button_inversionGris\" was not injected: check your FXML file 'edition_image.fxml'.";
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="button_inversionGris"
+    private Button button_inversionGris; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_symetrieHorizontale"
+    private Button button_symetrieHorizontale; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_refaire"
+    private Button button_refaire; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageView_editionImage"
+    private ImageView imageView_editionImage = new ImageView(); // Value injected by FXMLLoader
+
+
+    @FXML // fx:id="button_symetrieVerticale"
+    private Button button_symetrieVerticale; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_augmenterContraste"
+    private Button button_augmenterContraste; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_diminuerContraste"
+    private Button button_diminuerContraste; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_ajoutCommentaire"
+    private Button button_ajoutCommentaire; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_supprimer"
+    private Button button_supprimer; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_annuler"
+    private Button button_annuler; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_rotationGauche"
+    private Button button_rotationGauche; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_valider"
+    private Button button_valider; // Value injected by FXMLLoader
+
+    @FXML // fx:id="button_rotationDroite"
+    private Button button_rotationDroite; // Value injected by FXMLLoader
+
+    @FXML
+    private Slider slider_contrast = new Slider(0, 1, 1);
+
+    // @FXML
+    //  void 97979700(ActionEvent event) {
+    //
+    // }
+
+    @FXML
+    // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL location, ResourceBundle resources) {
+           /* assert button_inversionGris != null : "fx:id=\"button_inversionGris\" was not injected: check your FXML file 'edition_image.fxml'.";
             assert button_symetrieHorizontale != null : "fx:id=\"button_symetrieHorizontale\" was not injected: check your FXML file 'edition_image.fxml'.";
             assert button_refaire != null : "fx:id=\"button_refaire\" was not injected: check your FXML file 'edition_image.fxml'.";
             assert imageView_editionImage != null : "fx:id=\"imageView_editionImage\" was not injected: check your FXML file 'edition_image.fxml'.";
@@ -81,9 +96,143 @@ public class edition_image_controller {
             assert button_rotationGauche != null : "fx:id=\"button_rotationGauche\" was not injected: check your FXML file 'edition_image.fxml'.";
             assert button_valider != null : "fx:id=\"button_valider\" was not injected: check your FXML file 'edition_image.fxml'.";
             assert button_rotationDroite != null : "fx:id=\"button_rotationDroite\" was not injected: check your FXML file 'edition_image.fxml'.";
+*/
+    }
 
+    public ImageView getImageView_editionImage() {
+        return imageView_editionImage;
+    }
+
+    public void setImageView_editionImage(ImageView imageView_editionImage) {
+        this.imageView_editionImage = imageView_editionImage;
+    }
+
+    int verticalFlipCount = 0;
+
+    public void verticalFlipActivated(ActionEvent actionEvent) {
+
+        if (verticalFlipCount % 2 == 0) {
+            this.imageView_editionImage.setScaleY(-1.0);
+            System.out.println("verticalFlipActivated");
+        } else if (verticalFlipCount % 2 == 1) {
+            this.imageView_editionImage.setScaleY(1.0);
+            System.out.println("verticalFlipActivated again");
         }
+        verticalFlipCount++;
+    }
+
+    int horizontalFlipCount = 0;
+
+    public void horizontalFlipActivated(ActionEvent actionEvent) {
+        if (horizontalFlipCount % 2 == 0) {
+            this.imageView_editionImage.setScaleX(-1.0);
+            System.out.println("horizontalFlipActivated");
+        } else if (horizontalFlipCount % 2 == 1) {
+            this.imageView_editionImage.setScaleX(1.0);
+            System.out.println("horizontalFlipActivated again");
+        }
+        horizontalFlipCount++;
+    }
+
+    int rotateLeftCount = 0;
+
+    public void rotateLeftActivated(ActionEvent actionEvent) {
+        if (rotateLeftCount % 2 == 0) {
+            this.imageView_editionImage.rotateProperty().setValue(90);
+            System.out.println("rotate left");
+        } else if (rotateLeftCount % 2 == 1) {
+            this.imageView_editionImage.rotateProperty().setValue(0);
+            System.out.println("rotate left again");
+        }
+        rotateLeftCount++;
+
     }
 
 
+    int rotateRightCount = 0;
+
+    public void rotateRightActivated(ActionEvent actionEvent) {
+        if (rotateRightCount % 2 == 0) {
+            this.imageView_editionImage.rotateProperty().setValue(-90);
+            System.out.println("rotate right -90");
+        } else if (rotateRightCount % 2 == 1) {
+            this.imageView_editionImage.rotateProperty().setValue(-180);
+            System.out.println("rotate right -180");
+        }
+        rotateRightCount++;
+    }
+}
+
+
+/*this.slider_contrast.valueProperty().addListener(new ChangeListener<Number>() {
+        public void changed(ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) {
+            this.imageView_editionImage.setOpacity(new_val.doubleValue());
+
+        }
+    }); */
+
+  //this.slider_contrast.setBlockIncrement(-1);
+
+        /*private ColorAdjust colorAdjust;
+        private static final int ADJUST_TYPE_CONTRAST = 2;
+
+        Slider slider_contrast = this.createSlider(ADJUST_TYPE_CONTRAST);
+
+        // Create Slider to Adjust Color
+        private Slider createSlider(final int adjustType) {
+                Slider slider = new Slider();
+                slider.setMin(-1);
+                slider.setMax(1);
+                slider.setBlockIncrement(0.1);
+                slider.setValue(0);
+
+                slider.valueProperty().addListener(new ChangeListener<Number>() {
+
+                        @Override
+                        public void changed(ObservableValue<? extends Number> observable, //
+                                            Number oldValue, Number newValue) {
+                                switch (adjustType) {
+
+                                        case ADJUST_TYPE_CONTRAST:
+                                                colorAdjust.setContrast(newValue.doubleValue());
+                                                break;
+                                }
+                        }
+                });
+                return slider;
+        }
+*/
+
+/*
+
+
+
+
+    public ImageView loadImg() {
+
+        try {
+            Image newImg = SwingFXUtils.toFXImage(imgt, null);
+            imageView_editionImage.setImage(newImg);
+            rs.close();
+            stmnt.close();
+
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Not working");
+        }
+        return imageView_editionImage;
+    }
+
+
+    public void changeImage() {
+
+        Image newImg = SwingFXUtils.toFXImage(imgt, null);
+        img_1.setImage(newImg);
+...
+    } catch (Exception e) {
+        System.out.println("Not working");
+    }
+            return img_1;
+}*/
 
--- src/IU/edition_image/edition_image_main.java
+++ src/IU/edition_image/edition_image_main.java
@@ -1,15 +1,24 @@
 package IU.edition_image;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import sun.net.www.protocol.https.AbstractDelegateHttpsURLConnection;
+
+import static javafx.scene.image.Image.*;
 
 public class edition_image_main extends Application {
 
 
-
+        edition_image_controller edition_image = new edition_image_controller();
 
         @Override
         public void start(Stage primaryStage) throws Exception {
@@ -17,10 +26,17 @@ public class edition_image_main extends Application {
 
             FXMLLoader loader = new FXMLLoader(getClass().getResource("edition_image.fxml"));
             Parent root = loader.load();
+            primaryStage.setTitle("Edition d'Image");
             Scene scene = new Scene(root);
+            //edition_image_controller edition_d_image = new edition_image_controller();
+            final edition_image_controller edition_d_image = loader.getController();
+            //edition_d_image.getImageView_editionImage().setImage(new Image(getClass().getResourceAsStream("test_image_local/mri-abdomen11.jpg")));
             primaryStage.setScene(scene);
             primaryStage.show();
+
+
         }
+
 
         // main method to support non-JavaFX-aware environments:
 
