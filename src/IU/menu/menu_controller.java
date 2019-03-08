c020bd6c89ee89191d820d16a7a035d0b8bae71b
neilerua72 <neilerua72@gmail.com>
2019-03-08 09:49:30 +0100

    Ajout de la fonctionnalité de déconnexion

--- src/IU/menu/menu_controller.java
+++ src/IU/menu/menu_controller.java
@@ -1,11 +1,19 @@
 package IU.menu;
 
 import FC.SIR;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class menu_controller{
 
@@ -25,5 +33,18 @@ public class menu_controller{
     public void initData(SIR sir){
         nomprenom.setText(sir.getConnexion().getUser().getNomPremnom()+" / "+sir.getConnexion().getType().toString());
     }
+    public void seDeco(ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/IU/page_connection/page_connection.fxml"));
+        Parent conne = loader.load();
+        Scene scene = new Scene(conne);
+        Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+        System.out.println("deConnecter");
+
+    }
+
 
 }
